a6881e06c40a758fb1349dc1123b6719ec293512
HaasCP <57134208+HaasCP@users.noreply.github.com>
2022-03-03 13:47:58 +0100

    complete rework of gradient report

--- src/mocca/report/gradient.py
+++ src/mocca/report/gradient.py
@@ -10,52 +10,88 @@ import os
 import pandas as pd
 import datapane as dp
 
-from mocca.report.hplc_input import exps_to_df
 from mocca.dad_data.utils import sum_absorbance_by_time
 from mocca.visualization.basic_plots import plot_1D_data
 
 
-def report_gradient(gradient, report_path):
-    exps = [gradient.experiment]
-    exp_df = exps_to_df(exps)
+def gradients_to_df(gradients):
+    grad_dict = {'index': [],
+                 'file': [],
+                 'n_time_points': [],
+                 'method_time': [],
+                 'lambda_min': [],
+                 'lambda_max': []}
 
-    gradient_df = pd.DataFrame({
-        'time': gradient.time,
-        'absorbance': sum_absorbance_by_time(gradient.original_data)
-        })
-    gradient_plot = plot_1D_data(gradient_df, xlabel='Time (min)',
-                                 ylabel='Summed absorbance (mAU)',
-                                 title='', reduce_data=True)
-    baseline_df = pd.DataFrame({
-        'time': gradient.time,
-        'absorbance': sum_absorbance_by_time(gradient.data)
-        })
-    baseline_plot = plot_1D_data(baseline_df, xlabel='Time (min)',
-                                 ylabel='Summed absorbance (mAU)',
-                                 title='', reduce_data=True)
+    for i, grad in enumerate(gradients):
+        grad_dict['index'].append(i + 1)
+        grad_dict['file'].append(os.path.basename(grad.path))
+        grad_dict['n_time_points'].append(len(grad.time))
+        grad_dict['method_time'].append(round(max(grad.time), 2))
+        grad_dict['lambda_min'].append(min(grad.wavelength))
+        grad_dict['lambda_max'].append(max(grad.wavelength))
+    grad_df = pd.DataFrame(grad_dict)
+    return grad_df
 
-    grad_page = dp.Page(
+
+def create_gradient_pages(gradients):
+    grad_pages = []
+    for i, gradient in enumerate(gradients):
+        gradient_df = pd.DataFrame({
+            'time': gradient.time,
+            'absorbance': sum_absorbance_by_time(gradient.original_data)
+            })
+        gradient_plot = plot_1D_data(gradient_df, xlabel='Time (min)',
+                                     ylabel='Summed absorbance (mAU)',
+                                     title='', reduce_data=True)
+        baseline_df = pd.DataFrame({
+            'time': gradient.time,
+            'absorbance': sum_absorbance_by_time(gradient.data)
+            })
+        baseline_plot = plot_1D_data(baseline_df, xlabel='Time (min)',
+                                     ylabel='Summed absorbance (mAU)',
+                                     title='', reduce_data=True)
+        exp_df = gradients_to_df([gradient])
+
+        grad_page = dp.Page(
+            title=f"Gradient {i + 1}",
+            blocks=[
+                dp.Text("### Table: Gradient details."),
+                dp.Table(exp_df),
+                dp.Group(
+                    dp.Text("### Plot: Original gradient."),
+                    dp.Text("### Plot: Baseline of gradient."),
+                    columns=2),
+                dp.Group(
+                    dp.Plot(gradient_plot),
+                    dp.Plot(baseline_plot),
+                    columns=2)
+            ],
+        )
+        grad_pages.append(grad_page)
+    return grad_pages
+
+
+def report_gradients(exps, report_path):
+    gradients = []
+    for exp in exps:
+        if exp.gradient.dataset not in gradients:
+            gradients.append(exp.gradient.dataset)
+    grad_df = gradients_to_df(gradients)
+    grad_pages = create_gradient_pages(gradients)
+
+    summary_page = dp.Page(
         title="Start page",
         blocks=[
             dp.Group(
                 dp.Text("# Gradient report"),
                 dp.Text("## MOCCA (Multiway Online Chromatographic Chemical Analysis)"),
                 columns=2),
-            dp.Text("### Table: Gradient experiment as given by the user."),
-            dp.Table(exp_df, label="experiment_table"),
-            dp.Group(
-                dp.Text("### Plot: Original gradient."),
-                dp.Text("### Plot: Baseline of gradient."),
-                columns=2),
-            dp.Group(
-                dp.Plot(gradient_plot),
-                dp.Plot(baseline_plot),
-                columns=2)
+            dp.Text("### Table: Details to all gradients used in the campaign."),
+            dp.Table(grad_df)
         ],
     )
     r = dp.Report(
-        grad_page
+        summary_page,
+        *grad_pages
     )
     r.save(path=os.path.join(report_path, "report_gradient.html"), open=True)
-
-
