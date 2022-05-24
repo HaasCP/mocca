e4662c1752691962b2fa04a6414a48dd79b36238
HaasCP <57134208+HaasCP@users.noreply.github.com>
2022-05-24 13:39:43 +0200

    New robust adf read

--- src/mocca/dad_data/apis/allotrope.py
+++ src/mocca/dad_data/apis/allotrope.py
@@ -5,11 +5,38 @@ Created on Thu May 12 09:10:57 2022
 @author: CPH
 """
 
-import h5py
 import numpy as np
 import pandas as pd
 
 from mocca.dad_data.utils import df_to_array, apply_filter
+
+
+def get_uvvis_dataset_name(path):
+    
+    from h5ld import AllotropeDF
+    import h5py
+    import rdflib
+    
+    with h5py.File(path, mode="r") as f:
+        g = AllotropeDF(f).get_ld()
+    
+    datasets_query = '''SELECT ?s ?p ?o
+                        WHERE { ?s ?p ?o .
+                               FILTER regex(str(?o), "DataSet") .
+                               }'''
+    qres = g.query(datasets_query)
+
+    datasets = list(map(lambda x: x[0] , qres))
+    for d in datasets:
+        subj = list(g.triples((None, None, d)))[1][0]
+        dataset = list(
+            g.triples(
+            (subj, None, rdflib.term.URIRef('http://purl.allotrope.org/ontologies/result#AFR_0001527'))
+            )
+        )
+        if dataset:
+            dataset_name = d
+    return dataset_name
 
 
 def read_adf_datacube(path):
@@ -17,29 +44,32 @@ def read_adf_datacube(path):
     Reads the raw data stored in the data cube layer, which are the HPLC-DAD
     absorbance values and the time scale.
     """
-    f = h5py.File(path, 'r')
-    data_cubes = f['data-cubes']
-    uvvis_id = [idx for idx in data_cubes.keys() if "DAD1F" in idx][0]
-    uvvis_data = data_cubes[uvvis_id]
-    measures = uvvis_data['measures']
-    data_idx = list(measures.keys())[0]
-    absorbance_idx = measures[data_idx]
-    absorbance = absorbance_idx[()]
-    absorbance = np.swapaxes(absorbance, 0, 1)
+    import h5py
 
-    scales = uvvis_data['scales']
-    data_idx = list(scales.keys())[0]
-    time_idx = scales[data_idx]
-    time = time_idx[()]
-
+    with h5py.File(path, mode="r") as f:
+        data_cubes = f['data-cubes']
+        dataset_name = get_uvvis_dataset_name(path)
+        dataset_key = str(dataset_name).split(':')[1][2:].replace("/", '-')
+        uvvis_data = data_cubes[dataset_key]
+        measures = uvvis_data['measures']
+        data_idx = list(measures.keys())[0]
+        absorbance_idx = measures[data_idx]
+        absorbance = absorbance_idx[()]
+        absorbance = np.swapaxes(absorbance, 0, 1)
+    
+        scales = uvvis_data['scales']
+        data_idx = list(scales.keys())[0]
+        time_idx = scales[data_idx]
+        time = time_idx[()]
     return np.array(absorbance), list(time)
 
 
 def read_adf_description(wavelength_vals=None):
     """
     Queries the adf data description layer to extract the wavlength vector.
-    For this query, the h5ld package is required which cannot be used on Windows
-    machines currently. As a workaround, the user can give start and stop values
+    For this query, the h5ld package is required which can be installed by 
+    editable pip install from https://github.com/laura-dirocco/h5ld. In case there
+    are problems with installation, the user can give start and stop values
     as set on the DAD manually.
     """
     if wavelength_vals is not None:
@@ -47,7 +77,8 @@ def read_adf_description(wavelength_vals=None):
         wavelength = list(np.linspace(start, stop, length))
     else:
         try:
-            import h5ld  # noqa: F401
+            pass
+            # noqa: F401
             # to be developed with Laura di Rocco
         except AttributeError:
             print("If the h5ld package cannot be installed on your machine, "
