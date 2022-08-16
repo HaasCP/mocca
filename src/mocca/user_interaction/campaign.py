9bdb9c01c8a47f851c81fa83cf0c2880193bbaf3
HaasCP <57134208+HaasCP@users.noreply.github.com>
2022-08-16 15:07:20 +0200

    Update campaign docstring

--- src/mocca/user_interaction/campaign.py
+++ src/mocca/user_interaction/campaign.py
@@ -24,8 +24,8 @@ from mocca.report.main import report
 
 class HplcDadCampaign():
     """
-    Main parent class for HPLC-DAD experimental campaigns. We expect the
-    gradient to stay constant over the campaign.
+    Main class for HPLC-DAD campaigns containing all user input as well as
+    results from the data analysis.
     """
     def __init__(self, autosave_path=None):
         self.autosave_path = autosave_path
